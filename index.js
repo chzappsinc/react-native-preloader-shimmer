82a45b86167a2f0ca1211c7e39c741a116d6688f
Jasim Tk <65512279+chzappsinc@users.noreply.github.com>
2021-06-29 01:34:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,15 @@
 import React from 'react'
-import { NativeModules, View, Text, StyleSheet, ScrollView, ColorPropType } from 'react-native';
-import Shimmer from 'react-native-shimmer'
+import { View } from 'react-native';
+import { BasicLoader } from '@chzappsindia/react-native-loader'
 
-export const BasicLoader = ({
-    header = String,
-    color = ColorPropType,
-    type = require
-}) => {
-    console.log(color)
-    if (!color || color.length <= 0) {
-        console.warn("react-native-preolader-chz Says : Props color is required to run color={'Black'}")
-        return (null)
-    } else {
-        return (
-            <ScrollView showsVerticalScrollIndicator={false} style={{ padding: 20 }}>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
 
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
-                <Shimmer style={{ borderRadius: 10, marginBottom: 6 }}  >
-                    <View style={{ ...style.tip, backgroundColor: color }} />
-                </Shimmer>
+const App = () => {
 
-            </ScrollView>
-        )
-    }
+  return (
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <BasicLoader color={'#00000030'} />
+    </View>
+  )
 }
 
-const style = StyleSheet.create({
-    tip: {
-        borderRadius: 10,
-        backgroundColor: '#ffffff50',
-        height: 20,
-        paddingBottom: 2,
-        paddingTop: 2,
-        marginBottom: 5
-    }
-})
+export default App;
