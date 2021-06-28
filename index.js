05e7a92f37d34f1303c06b049940b48abe857672
Jasim Tk <65512279+chzappsinc@users.noreply.github.com>
2021-06-29 04:22:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { View } from 'react-native';
-import { BasicLoader } from '@chzappsindia/react-native-loader'
+import { View, StyleSheet } from 'react-native';
+import { MainLoader } from 'react-native-preloader-shimmer'
 
 
 const App = () => {
 
   return (
     <View style={{ flex: 1, backgroundColor: 'white' }}>
-      <BasicLoader color={'#00000030'} />
+      <MainLoader barStyle={'dark-content'} animSpeed={100} visible={true} backgroundColor={'white'} />
     </View>
   )
 }
